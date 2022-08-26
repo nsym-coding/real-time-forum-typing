97d9866c8ae446b9e67f335d79b5084982713aa8
eternal17 <sarmadkhatri15@gmail.com>
2022-08-26 15:57:18 +0100

    moving over files

--- main.go
+++ main.go
@@ -1,26 +1,127 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"real-time-forum/db"
 
 	"github.com/gorilla/websocket"
 )
+
+type T struct {
+	TypeChecker
+	*Posts
+	*Comments
+}
+
+type TypeChecker struct {
+	Type string `json:"type"`
+}
+
+type Posts struct {
+	Title       string `json:"title"`
+	PostContent string `json:"postcontent"`
+	Date        string `json:"posttime"`
+	Tipo        string `json:"tipo"`
+	User        string `json:"user"`
+}
+
+type Comments struct {
+	CommentContent string `json:"commentcontent"`
+	User           string `json:"user"`
+	Date           string `json:"commenttime"`
+	Tipo           string `json:"tipo"`
+}
+
+var clients = make(map[*websocket.Conn]bool)
+var broadcastChannelPosts = make(chan *Posts, 1)
+var broadcastChannelComments = make(chan *Comments, 1)
+
+// unmarshall data based on type
+func (t *T) UnmarshalForumData(data []byte) error {
+	if err := json.Unmarshal(data, &t.TypeChecker); err != nil {
+		log.Println("Error when trying to sort forum data type...")
+	}
+
+	switch t.Type {
+	case "post":
+		t.Posts = &Posts{}
+		return json.Unmarshal(data, t.Posts)
+	case "comment":
+		t.Comments = &Comments{}
+		return json.Unmarshal(data, t.Comments)
+	default:
+		return fmt.Errorf("unrecognized type value %q", t.Type)
+	}
+}
 
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
 }
 
-func wsEndPoint(w http.ResponseWriter, r *http.Request) {
-
+func webSocketEndpoint(w http.ResponseWriter, r *http.Request) {
+	go broadcastToAllClients()
 	wsConn, err := upgrader.Upgrade(w, r, nil)
+
 	if err != nil {
-		log.Println("Error upgrading clients connection...")
+		log.Println("error when upgrading connection...")
+	}
+
+	defer wsConn.Close()
+
+	clients[wsConn] = true
+
+	for {
+		_, infoType, _ := wsConn.ReadMessage()
+		var f T
+		f.UnmarshalForumData(infoType)
+
+		if f.Type == "post" {
+			f.Posts.Tipo = "post"
+			f.Posts.User = "yonas"
+			broadcastChannelPosts <- f.Posts
+		} else if f.Type == "comment" {
+			f.Comments.User = "yonas"
+			f.Comments.Tipo = "comment"
+
+			broadcastChannelComments <- f.Comments
+		}
+
+		log.Println("Checking what's in f ---> ", f)
 	}
 
 }
 
-func main() {
+func broadcastToAllClients() {
 
+	for {
+		select {
+		case x, ok := <-broadcastChannelPosts:
+			if ok {
+				for client := range clients {
+					client.WriteJSON(x)
+					fmt.Printf("Value %v was read.\n", x)
+				}
+			}
+		case y, ok := <-broadcastChannelComments:
+			if ok {
+				for client := range clients {
+					client.WriteJSON(y)
+				}
+			}
+		}
+	}
+}
+
+func main() {
+	db.CreateDB()
+	// need to figure out how to serve these files on the homepage (/)
+	fs := http.FileServer(http.Dir("./"))
+	http.Handle("/", fs)
+	http.HandleFunc("/ws", webSocketEndpoint)
+	log.Println("Listening on port :3000.....")
+	log.Fatal(http.ListenAndServe(":3000", nil))
 }
--- static/js/app.js
+++ static/js/app.js
@@ -10,3 +10,73 @@ let postContent = document.getElementById("post-content-text");
 let postTitle = document.getElementById("post-title-text");
 let postDisplay = document.getElementById("post-feed");
 let content = document.getElementById("content")
+let msgArr = [];
+let objData = {};
+let commentData = {};
+let sendingArr = [];
+
+
+// client side websocket
+ws.onopen = () => {
+    console.log("Connection to server established...");
+};
+
+
+submitPostButton.addEventListener("click", function (e) {
+    e.preventDefault();
+    console.log("submit---", postButton);
+    postButton.style.display = "block";
+    postModal.style.display = "none";
+
+    objData["title"] = postTitle.value;
+    objData["postcontent"] = postContent.value;
+    objData["type"] = "post"
+    objData["posttime"] = new Date().toISOString().slice(0, 10)
+
+    postTitle.value = ''
+    postContent.value = ''
+
+    // message sent to server
+    ws.send(JSON.stringify(objData));
+
+});
+
+// message received from server side
+ws.onmessage = (e) => {
+    let data = JSON.parse(e.data);
+    console.log(data.tipo);
+    console.log(data);
+
+    if (data.tipo === "post") {
+        msgArr.push(data.title + "\n" + data.postcontent + "\n" + data.user + " " + data.posttime);
+        content.textContent += "\n" + msgArr[msgArr.length - 1];
+    }
+
+    if (data.tipo === "comment") {
+        alert(`COmment received${data.commentcontent}`)
+    }
+    console.log("Received this message from server....", data);
+
+};
+
+submitCommentButton.addEventListener("click", function (e) {
+    e.preventDefault();
+    postButton.style.display = "block";
+    postModal.style.display = "none";
+
+    commentData["commentcontent"] = commentContent.value
+    commentData["type"] = "comment"
+    commentData["commenttime"] = new Date().toISOString().slice(0, 10)
+
+    commentContent.value = ''
+    ws.send(JSON.stringify(commentData));
+})
+
+
+postButton.addEventListener("click", function (e) {
+    e.preventDefault();
+    console.log("post---", postButton);
+
+    postButton.style.display = "none";
+    postModal.style.display = "block";
+});
