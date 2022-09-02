29922c15dd87fca9b6bc1fa4dceba2c1507cbcbe
Arnold Mutungi <abmutungi@gmail.com>
2022-09-02 14:29:54 +0100

    folder-structure

--- users/users.go
+++ users/users.go
@@ -1,20 +1,20 @@
 package users
 
 import (
-	"database/sql"
 	"fmt"
+	"real-time-forum/server"
 )
 
 // this func registers a users username, email, firstname, lastname, password(unhashed) and age
-func RegisterUser(username string, hash []byte, email string) {
-	db, _ := sql.Open("sqlite3", "real-time-forum.db")
-	stmt, err := db.Prepare("INSERT INTO users (username, hash, email) VALUES (?, ?, ?)")
+func (s *MyServer) RegisterUser(username string, age int, firstname string, lastname string, hash []byte, email string) {
+	// db, _ := sql.Open("sqlite3", "real-time-forum.db")
+	stmt, err := s.Db.Prepare("INSERT INTO users (username, age, firstname, lastname, hash, email) VALUES (?, ?, ?, ?, ?, ?)")
 	if err != nil {
 		fmt.Println("error preparing statement:", err)
 		return
 	}
 	// defer stmt.Close()
-	result, _ := stmt.Exec(username, hash, email)
+	result, _ := stmt.Exec(username, age, firstname, lastname, hash, email)
 	// checking if the result has been added and the last inserted row
 	rowsAff, _ := result.RowsAffected()
 	lastIns, _ := result.LastInsertId()
