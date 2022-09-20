f806ff4b64e9f6c3f50e0861b63b9cfa036d8eda
nsym_coding <nsym.coding@outlook.com>
2022-09-20 14:39:33 +0100

    added and positioned empty chat box

--- static/newapp.js
+++ static/newapp.js
@@ -30,8 +30,10 @@ for (let i = 0; i < 10; i++) {
   posts.appendChild(postDivs);
 }
 
+let userDetails;
+
 for (let i = 0; i < 4; i++) {
-  let userDetails = document.createElement("button");
+  userDetails = document.createElement("div");
   let username = document.createElement("div");
   let imageDiv = document.createElement("div");
   let img = document.createElement("img");
@@ -63,3 +65,40 @@ Array.from(postTitlesClick).forEach(function (postTitle) {
     alert(postTitle.innerText);
   });
 });
+
+var modal = document.querySelector(".modal");
+
+var userRg = document.querySelectorAll(".registered-user")
+
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+
+for (let i = 0; i<userRg.length; i++){
+  userRg[i].onclick = function() {
+    console.log("Users clicked");
+    modal.style.display = "block";
+  }
+}
+
+// userRg.onclick = function() {
+//   console.log("Users clicked");
+//   modal.style.display = "block";
+// }
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
